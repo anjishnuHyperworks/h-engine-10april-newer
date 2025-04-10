f8dda35553b9ea529e3260387931bc35037d41e3
anjishnuHyperworks <anjishnu@hyperworksimaging.com>
2025-04-10 20:52:20 +0530

    update server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
 app.get('/', (req, res) => {
     res.send('H-Engine API is running');
   });
