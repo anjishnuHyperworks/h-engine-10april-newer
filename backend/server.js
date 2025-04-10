fdd391ac762f8e0b564c6c818c6cb7f706620c62
anjishnuHyperworks <anjishnu@hyperworksimaging.com>
2025-04-10 21:01:48 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,6 @@ const allowedOrigins = [
     "http://localhost:3000", // LOCAL DEVELOPMENT
     "https://h-engine-10april-newer.vercel.app",  // removed trailing slash
     "https://h-engine-10april-newer-anjishnuhyperworks-projects.vercel.app",  // added second domain
-    "*"
 ];
 
 // Add this before any middleware
